c2fbd90b21a434125b0450b0ef7a57bdec777a4b
adityasriv2317 <adityaxia9237@gmail.com>
2024-10-24 10:38:36 +0530

    contact field

--- src/component/contact.jsx
+++ src/component/contact.jsx
@@ -7,6 +7,20 @@ const contact = () => {
         <hr />
         <div className="moto">Contact Me</div>
 
+        <div className="contactMe">
+          <div className="cdetails">
+            <h2><b>Email :</b> adityaxia9237@gmail.com</h2>
+            <h2><b>Number :</b> +91 1234567890</h2>
+          </div>
+
+          <div className="send">
+            <h1>Submit a feedback</h1>
+            <form action="">
+              <label htmlFor="umail"></label>
+              <input type="email" name='umail' id='umail'/>
+            </form>
+          </div>
+        </div>
     </div>
   )
 }
